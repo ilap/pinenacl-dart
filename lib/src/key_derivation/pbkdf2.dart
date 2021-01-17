000fe788415197b877ecffce57c4c71c22cb6ae5
Pal Dorogi <pal.dorogi@gmail.com>
2021-01-17 19:34:45 +1000

    Added PBKDF2-HMAC-SGA256 and refactored PBKDF2.

--- lib/src/key_derivation/pbkdf2.dart
+++ lib/src/key_derivation/pbkdf2.dart
@@ -1,9 +1,7 @@
 import 'dart:typed_data';
 
-import 'package:pinenacl/src/message_authentication/hmac.dart';
+import 'package:pinenacl/tweetnacl.dart';
 import 'package:pinenacl/src/utils/utils.dart';
-
-typedef Hasher = void Function(Uint8List out, Uint8List text, Uint8List k);
 
 ///
 /// PBKDF2 (RFC 2898) is a cryptographic key derivation function, which is
@@ -13,21 +11,26 @@ typedef Hasher = void Function(Uint8List out, Uint8List text, Uint8List k);
 /// hmac (currently only HMAC-SHA512) with a cryptographically secure `salt`.
 ///
 class PBKDF2 {
-  // TODO: currently only HMAC-SHA512 is implemented.
+  // TODO: currently only HMAC-SHA-512 and HMAC-SHA-256 are implemented.
   static Uint8List hmac_sha512(
       Uint8List password, Uint8List salt, int count, int key_length) {
-    var hasher = Hmac.sha512;
-    return _deriveKey(hasher, password, salt, count, key_length);
+    var hasher = TweetNaClExt.crypto_auth_hmacsha512;
+    return _deriveKey(hasher, 64, password, salt, count, key_length);
   }
 
-  static Uint8List _deriveKey(Hasher hasher, Uint8List password, Uint8List salt,
-      int count, int key_length) {
+  static Uint8List hmac_sha256(
+      Uint8List password, Uint8List salt, int count, int key_length) {
+    var hasher = TweetNaClExt.crypto_auth_hmacsha256;
+    return _deriveKey(hasher, 32, password, salt, count, key_length);
+  }
+
+  static Uint8List _deriveKey(MacHasher hasher, int hash_length,
+      Uint8List password, Uint8List salt, int count, int key_length) {
     if (count <= 0 || key_length < 1 || key_length > 0xffffffff) {
       throw Exception();
     }
 
-    final hash_length = 64;
-    final block_count = (key_length / hash_length).round();
+    final block_count = (key_length / hash_length).ceil();
 
     final derived_key = Uint8List(key_length + hash_length);
     final _U = Uint8List(hash_length);
@@ -43,14 +46,14 @@ class PBKDF2 {
 
       final message = Uint8List.fromList([...salt, ...idx]);
 
-      Hmac.sha512(_U, message, password);
+      hasher(_U, message, password);
 
       final offset = (i - 1) * hash_length;
 
       PineNaClUtils.listCopy(_U, hash_length, derived_key, offset);
 
       for (var j = 1; j < count; j++) {
-        Hmac.sha512(_U, _U, password);
+        hasher(_U, _U, password);
 
         for (var k = 0; k < hash_length; k++) {
           derived_key[k + offset] ^= _U[k];
