6c6f034cc94f6760f57115fe8085d1213d1e0678
Pal Dorogi <pal.dorogi@gmail.com>
2021-01-18 20:35:00 +1000

    Still refactoring

--- benchmark/hashing_benchmark.dart
+++ benchmark/hashing_benchmark.dart
@@ -7,11 +7,18 @@ Original JIT - order is important for stopwatch.
 100000 of SHA-256 took: 0.228
 100000 of BLAKE2B took: 0.626
 100000 of SHA-512 took: 2.775
+100000 iterations of SHA-256 took 0.23 sec(s)  // after tweaked tweetnacl a littlebit. 18/01/2021
+100000 iterations of BLAKE2B took 0.623 sec(s) // after tweaked tweetnacl a littlebit. 18/01/2021
+100000 iterations of SHA-512 took 1.686 sec(s) // after tweaked tweetnacl a littlebit. 18/01/2021
 
 Original AOT
 100000 of SHA-256 took: 2.511
 100000 of SHA-512 took: 10.399
 100000 of BLAKE2B took: 10.763
+100000 iterations of SHA-256 took 0.716 sec(s) // after tweaked tweetnacl a littlebit. 18/01/2021
+100000 iterations of BLAKE2B took 1.079 sec(s) // after tweaked tweetnacl a littlebit. 18/01/2021
+100000 iterations of SHA-512 took 2.842 sec(s)  // after tweaked tweetnacl a littlebit. 18/01/2021
+
 */
 
 void _printMessage(String alg, int iter, double sec) =>
